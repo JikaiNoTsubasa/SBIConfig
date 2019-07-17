260b683db1ab87e5a2fc5f9a96242920f84e1970
Stephane Biehler <stephane.biehler.priv@gmail.com>
2019-07-17 15:38:14 +0200

    Updated test

--- src/fr/triedge/config/test/Test.java
+++ src/fr/triedge/config/test/Test.java
@@ -7,7 +7,7 @@ import fr.triedge.config.SBIConfig;
 public class Test {
 
 	public static void main(String[] args) {
-		SBIConfig c = new SBIConfig("test/conf");
+		SBIConfig<String, String> c = new SBIConfig<String,String>("test/conf");
 		c.add("fr.triedge.conf.thread.count", "5");
 		try {
 			c.saveConfig();
